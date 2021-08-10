1fa679939d0aef699ef66883ebb562b1b347a277
DockBoss <jareddoro@gmail.com>
2021-08-10 13:35:26 -0500

    wip1 starting out

---
+++ madLibs/madLibs.py
@@ -0,0 +1,6 @@
+madLib = ["It was a adj, cold November day. I woke up to the adj smell of", "brd roasting in the rom downstairs.", "I ptv down the stairs to see if I could help vrb the dinner.", "My Mom said, 'See if rel needs a fresh non.'", "So I carried a tray of glasses full of lqd into", " the vng room. When I got there, I couldn't believe my pbp!", "There were pln vng on the non!"]
+print("Welcome to PyLibs!")
+
+for x in madLib: 
+    print(x)
+    
