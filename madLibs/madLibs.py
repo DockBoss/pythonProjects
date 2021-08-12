1ec550317d5c13e899d5ae5ea616fcd500b3d652
BIGGbraxton <braxtonmacklin5@gmail.com>
2021-08-12 19:30:07 -0400

    madlibs dict

--- madLibs/madLibs.py
+++ madLibs/madLibs.py
@@ -14,7 +14,7 @@ pbp = input("name a part of the body: ")
 pln = input("name a plural noun: ")
 vng2 = input("name an ing verb: ")
 non2 = input("write a noun: ")
-madLib = "It was a "+ adj +", cold November day. I woke up to the "+ adj2 +" smell of"+ brd +"roasting in the "+ rom +" downstairs. I "+ ptv +" down the stairs to see if I could help "+ vrb + " the dinner. My Mom said, 'See if "+ rel +" needs a fresh "+ non +".' So I carried a tray of glasses full of "+ lqd +" into the "+ vng +" room. When I got there, I couldn't believe my "+ pbp +"! There were "+ pln +" "+ vng +" on the "+ non2+ "!"
+madLib = "It was a "+ adj +", cold November day. I woke up to the "+ adj2 +" smell of"+ brd +"roasting in the "+ rom +" downstairs. I "+ ptv +" down the stairs to see if I could help "+ vrb + " the dinner. My Mom said, 'See if "+ rel +" needs a fresh "+ non +".' So I carried a tray of glasses full of "+ lqd +" into the "+ vng +" room. When I got there, I couldn't believe my "+ pbp +"! There were "+ pln +" "+ vng +" on the "+ non2+"!"
 print("Welcome to PyLibs!")
 
 print(madLib)
---
+++ madLibs/madLibsDict.py
@@ -0,0 +1,25 @@
+# madLib generator using a dictionary
+import sys
+
+Word_dict = {
+    'noun'  :
+    'vng'   :
+    'adj'   : 
+    'emtn'  : 
+    'actn'  :
+    'friend': 
+}
+
+
+
+
+
+madLib = (
+    "One Valentine's "+ noun +" I was "+ vng +", when I looked in my "+ noun +" and saw a "+ adj +" "+ noun +" !"
+    "It said, 'Will you be my "+ noun +"?' I was so "+ emtn +"! I "+ actn +" to see who it was from, but there was"
+    "no "+ noun +". So, at "+ noun +", I asked for clues, but "+ friend +" didn't know about it. Finally, someone"
+    "told me that "+ friend +" gave me the "+ noun +"."
+)
+
+print(madLib)
+
