2fd96d8a9c202a826ff1f482b3fdbc491e802644
Jared Doro <56698307+DockBoss@users.noreply.github.com>
2021-12-27 19:55:38 -0800

    messed around for a little bit

--- snake/main.py
+++ snake/main.py
@@ -28,7 +28,13 @@ class Food(pygame.sprite.Sprite):
 		self.body = pygame.Surface((10, 10))
 		self.body.fill((255, 255, 255))
 		self.rect = self.body.get_rect()
+		#self.position = tuple(round(random.random() * 500, -1) for _ in range(2))
 
+#created snake body
+class Body(pygame.sprite.Sprite):
+	def __init__(self):
+		super(Body, self).__init__()
+		self.body = pygame.surface
 
 # create snake claas
 class Snake(pygame.sprite.Sprite):
@@ -38,17 +44,16 @@ class Snake(pygame.sprite.Sprite):
 		self.body.fill((255, 255, 255))
 		self.rect = self.body.get_rect()
 
+
 	# set up movements for snake
-	def update(self, pressed_key):
-		# TODO Right not you can't override up because the elifs, but without them it allows you to move diagonally
-		# so fix movement
-		if pressed_key[K_UP or K_w]:
+	def update(self, direction):
+		if direction == 1:
 			self.rect.move_ip(0, -5)
-		elif pressed_key[K_LEFT or K_a]:
+		elif direction == 4:
 			self.rect.move_ip(-5, 0)
-		elif pressed_key[K_RIGHT or K_d]:
+		elif direction == 2:
 			self.rect.move_ip(5, 0)
-		elif pressed_key[K_DOWN or K_s]:
+		elif direction == 3:
 			self.rect.move_ip(0, 5)
 
 		# Kill snake when it hits a wall
@@ -63,30 +68,33 @@ class Snake(pygame.sprite.Sprite):
 
 
 game_speed = pygame.time.Clock()
-
 SCREEN_HEIGHT = 500
 SCREEN_WIDTH = 500
 # Set up the drawing window
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
 # create groups for entiteis
-food_group = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 
 
 # Create a custom event for adding a new enemy
-ADDFOOD = pygame.USEREVENT + 1
-ADDEVENT = pygame.event.Event(ADDFOOD)
-#	pygame.time.set_timer(ADDFOOD, 2300)
-# create instance of snake and add it to the group
+#TODO ADDFOOD = pygame.USEREVENT + 1
+#TODO ADDEVENT = pygame.event.Event(ADDFOOD)
+
+#create instance of snake and add it to the group
 python = Snake()
 all_sprites.add(python)
 
+#create an instance of food and add it to the group
+food = Food()
+all_sprites.add(food)
+
+direction = -1  # direction of snake -1 to start, so it won't move until a keypress
 
 # Run until the user asks to quit
 running = True
 while running:
-	pygame.event.post(ADDEVENT)
+	#TODO pygame.event.post(ADDEVENT)
 	# Did the user click the window close button?
 	for event in pygame.event.get():
 		if event.type == KEYDOWN:
@@ -95,24 +103,45 @@ while running:
 		# Check for QUIT event. If QUIT, then set running to false.
 		elif event.type == QUIT:
 			running = False
-		elif event.type == ADDFOOD:
+		"""elif event.type == ADDFOOD:
+			food.kill()
 			food = Food()
-			food_group.add(food)
+			print('didi it work')
 			all_sprites.add(food)
+	Will not work, snake is always colliding with itself
+	if pygame.sprite.collide_rect(python, python):
+		python.kill()
+		running = False
+	"""
 
-	ate_food = pygame.sprite.collide_rect(python, food)
-	if ate_food:
+	if pygame.sprite.collide_rect(python, food):
 		print('ate food')
+		# pygame.event.post(ADDFOOD)
+
 
 	pressed_keys = pygame.key.get_pressed()
+	#set direction baised on key presses
+	if pressed_keys[K_UP] or pressed_keys[K_w]:
+		if direction != 3:
+			direction = 1
+	elif pressed_keys[K_LEFT] or pressed_keys[K_a]:
+		if direction != 2:
+			direction = 4
+	elif pressed_keys[K_RIGHT] or pressed_keys[K_d]:
+		if direction != 4:
+			direction = 2
+	elif pressed_keys[K_DOWN] or pressed_keys[K_s]:
+		if direction != 1:
+			direction = 3
+	python.update(direction)
 
-	python.update(pressed_keys)
 	# if you don't fill after each move the snake works as a paint brush
-	# This is how I will make the snake move
-	# I will only have to paint over the tail of snake each time not the entire screen
 	screen.fill((0, 0, 0))
+
 	for thing in all_sprites:
 		screen.blit(thing.body, thing.rect)
+
+	# update screen
 	pygame.display.flip()
 
 	game_speed.tick(30)
--- snake/methods.md
+++ snake/methods.md
@@ -6,6 +6,9 @@ I am using this as a think pad to fix current problems in my snake project.
 I need to edit the movements of the snake so it is always moving and when there is no key pressed.
 1. abstract the movements from snake class and add it to the main loop
 2. or just edit how the movements are read in and adjust the update function of snake accordinly 
+#### Solution
+Used #2 checked the pressed keys in the main loop sent an int to the function that told what direction to move the Snake in the update function in snake.
+When no valid key is pressed it will just keep the last direction used.
 ### Resizing
 I need to find a way to resize the sprite.
 1. See if you can keep adding rect's to the sprite
@@ -14,16 +17,21 @@ I need to find a way to resize the sprite.
     * Might need to create a function to help placing the body on the screen
         * just remember the side that the trailing sprite was connected to
     * This will also make collision detecton really easy.
+#### Solution
 ### collision detecton
 Need to figure out how to tell when the game is over
 1. Not sure how I would handle it in this case.
 2. .kill the snake when it collides with the body and end the game
     * Maybe I don't need to .kill the snake or body at all. Just end the game loop
-
+#### Solution
 
 ## Food
 ### generating food
+still collides with snake when food is not even on the screen
+
 look into pygame events more
 
 look into groupSingle
-### randomly placing food                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+#### Solution
+### randomly placing food         
+#### Solution                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
